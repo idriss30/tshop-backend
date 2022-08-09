c13e09b19f7ebb3185eca69cc6ae53237510ff36
idriss30 <idriss.cissoko@hotmail.com>
2022-08-09 15:20:45 -0400

    creating productList seed for database

---
+++ database/productsData.js
@@ -0,0 +1,185 @@
+module.exports = [
+  {
+    category: "man",
+    bestSeller: true,
+    name: "ZIP MOCK NECK T-SHIRT",
+    description: "Mock neck T-shirt with front zip closure and short sleeves",
+    price: 40,
+    imageName: "zipBlack",
+  },
+  {
+    category: "man",
+    bestSeller: false,
+    name: "ZIP MOCK NECK T-SHIRT",
+    description: "Mock neck T-shirt with front zip closure and short sleeves",
+    price: 40,
+    imageName: "zipBlue",
+  },
+  {
+    category: "man",
+    bestSeller: false,
+    name: "ZIP MOCK NECK T-SHIRT",
+    description: "Mock neck T-shirt with front zip closure and short sleeves",
+    price: 40,
+    imageName: "zipGreen",
+  },
+  {
+    category: "man",
+    bestSeller: false,
+    name: "ZIP MOCK NECK T-SHIRT",
+    description: "Mock neck T-shirt with front zip closure and short sleeves",
+    price: 40,
+    imageName: "zipWhite",
+  },
+
+  {
+    category: "man",
+    bestSeller: true,
+    name: "COMBINATION PRINT T-SHIRT",
+    description:
+      "Round neck T-shirt with short sleeves. All-over contrasting print details.",
+    price: 40,
+    imageName: "combination",
+  },
+  {
+    category: "man",
+    bestSeller: false,
+    name: "COMBINATION BASKETBALL T-SHIRT",
+    description:
+      "Round neck T-shirt with short sleeves. Contrasting graphic prints at front and back.",
+    price: 45,
+    imageName: "print",
+  },
+  {
+    category: "man",
+    bestSeller: false,
+    name: "PRINT T-SHIRT PARIS",
+    description:
+      "Round neck T-shirt with short sleeves. Contrasting graphic prints at front and back.",
+    price: 50,
+    imageName: "paris",
+  },
+  {
+    category: "man",
+    bestSeller: false,
+    name: "EMBROIDERED SHIRT",
+    description:
+      "Round neck T-shirt with short sleeves. Contrasting graphic prints at front and back.",
+    price: 40,
+    imageName: "embroidered",
+  },
+  {
+    category: "man",
+    bestSeller: true,
+    name: "TEXT PRINT T-SHIRT",
+    description:
+      "Round neck T-shirt with short sleeves. Contrasting graphic prints at front and back.",
+    price: 40,
+    imageName: "black",
+  },
+  {
+    category: "man",
+    bestSeller: false,
+    name: "Graphic PRINT T-SHIRT",
+    description:
+      "Round neck T-shirt with short sleeves. Contrasting graphic prints at front and back.",
+    price: 40,
+    imageName: "graphic",
+  },
+  {
+    category: "man",
+    bestSeller: false,
+    name: "Graphic PRINT T-SHIRT",
+    description:
+      "Round neck T-shirt with short sleeves. Contrasting graphic prints at front and back.",
+    price: 40,
+    imageName: "taupe",
+  },
+  {
+    category: "man",
+    bestSeller: false,
+    name: "Graphic PRINT T-SHIRT",
+    description:
+      "Round neck T-shirt with short sleeves. Front contrast prints of embroidery.",
+    price: 40,
+    imageName: "black2",
+  },
+  {
+    category: "woman",
+    bestSeller: true,
+    name: "VENTED COTTON T-SHIRT",
+    description:
+      "T-shirt made of 100% cotton. Round neck and short sleeves. Side vents at hem.",
+    price: 32,
+    imageName: "ventedBlack",
+  },
+  {
+    category: "woman",
+    bestSeller: false,
+    name: "VENTED COTTON T-SHIRT",
+    description:
+      "T-shirt made of 100% cotton. Round neck and short sleeves. Side vents at hem.",
+    price: 32,
+    imageName: "ventedBlue",
+  },
+  {
+    category: "woman",
+    bestSeller: false,
+    name: "VENTED COTTON T-SHIRT",
+    description:
+      "T-shirt made of 100% cotton. Round neck and short sleeves. Side vents at hem.",
+    price: 32,
+    imageName: "ventedTaupe",
+  },
+  {
+    category: "woman",
+    bestSeller: true,
+    name: "SHORT SLEEVE RIBBED T-SHIRT",
+    description: "Round neck T-shirt with short sleeves. Scalloped trim",
+    price: 32,
+    imageName: "second",
+  },
+  {
+    category: "woman",
+    bestSeller: false,
+    name: "SHORT SLEEVE RIBBED T-SHIRT",
+    description: "Round neck T-shirt with short sleeves. Scalloped trim",
+    price: 32,
+    imageName: "third",
+  },
+  {
+    category: "woman",
+    bestSeller: false,
+    name: "SHORT SLEEVE RIBBED T-SHIRT",
+    description: "Round neck T-shirt with short sleeves. Scalloped trim",
+    price: 32,
+    imageName: "fourth",
+  },
+  {
+    category: "woman",
+    bestSeller: true,
+    name: "T-SHIRT WITH BUTTONS",
+    description:
+      "Round neck T-shirt with short sleeves. Side metal button detail.",
+    price: 30,
+    imageName: "woman",
+  },
+  {
+    category: "woman",
+    bestSeller: false,
+    name: "V-NECK T-SHIRT",
+    description:
+      "T-shirt made of 100% cotton with V-neck and short sleeves with dropped shoulders.",
+    price: 30,
+    imageName: "vneck",
+  },
+  {
+    category: "woman",
+    bestSeller: false,
+    name: "WASHED CROP TOP",
+    description:
+      "Due to the dyeing process, the print on each item is unique and special and may differ slightly from the one pictured.",
+    price: 30,
+    imageName: "washed",
+  },
+];
