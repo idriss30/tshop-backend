19e43033ad51466995a7e25d65973b42919cdb91
idriss30 <idriss.cissoko@hotmail.com>
2022-08-09 17:18:17 -0400

    setting up test database

--- database/config/db.config.js
+++ database/config/db.config.js
@@ -1,9 +1,10 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
+const environment = process.env.ENV || "development";
 // create the connection
 let sequelize;
-if (process.env.ENV === "production") {
+if (environment === "production") {
   sequelize = new Sequelize(
     process.env.DATABASE__NAME,
     process.env.DATABASE__USER,
@@ -15,7 +16,7 @@ if (process.env.ENV === "production") {
     }
   );
 } else {
-  sequelize = new Sequelize("test", "test", "test", {
+  sequelize = new Sequelize("dozotest", "root", "rootpassword", {
     host: "localhost",
     dialect: "mysql",
     logging: false,
