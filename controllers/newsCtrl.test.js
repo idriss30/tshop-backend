9549d757c353e3b04c8ff38cacdb2e75bbd9ea7d
idriss30 <idriss.cissoko@hotmail.com>
2022-08-09 20:46:14 -0400

    added test for newsLetter Component

---
+++ controllers/newsCtrl.test.js
@@ -0,0 +1,22 @@
+const request = require("supertest");
+const app = require("../app");
+
+test("submitting valid post request to newsLetter Route", async () => {
+  const postResponse = await request(app)
+    .post("/api/news/email")
+    .send({ email: "email@test.com" })
+    .expect(201);
+
+  expect(postResponse.body).toEqual({ message: "email was saved" });
+});
+
+test("submitting existing email  to post request", async () => {
+  await request(app).post("/api/news/email").send({ email: "email@test.com" });
+
+  const badResponse = await request(app)
+    .post("/api/news/email")
+    .send({ email: "email@test.com" })
+    .expect(400);
+
+  expect(badResponse.body).toEqual({ error: "email in use" });
+});
