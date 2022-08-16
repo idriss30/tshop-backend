b9361cebcb16712abf67ca1080cc90c5599d6597
idriss30 <idriss.cissoko@hotmail.com>
2022-08-16 15:06:11 -0400

    added modules to setupFilesAfterEnv

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   testEnvironment: "node",
-
+  globalSetup: "<rootDir>/database/globalSetup.js",
   setupFilesAfterEnv: [
     "jest-extended",
-    /* "<rootDir>/database/migrateDatabase.js",
-    "<rootDir>/database/truncateTables.js",
-    "<rootDir>/database/disconnectDb.js", */
+    "<rootDir>/database/migrateDatabase.js",
+    //"<rootDir>/database/truncateTables.js",
+    "<rootDir>/database/disconnectDb.js",
   ],
 };
