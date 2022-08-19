be9fc10d8e98c976fc57bb515469e981632855bc
idriss30 <idriss.cissoko@hotmail.com>
2022-08-19 19:00:51 -0400

    auth.js and test refactored

--- controllers/middleware/auth.js
+++ controllers/middleware/auth.js
@@ -1,12 +1,12 @@
 const { decodeToken } = require("../userReusable");
 require("dotenv").config();
 
-const checkAuth = (req, res, next) => {
+const checkAuth = async (req, res, next) => {
   try {
     const tokenCookie = req.cookies.token;
     const decodedToken = decodeToken(tokenCookie);
     req.userId = decodedToken;
-    next();
+    return await next();
   } catch (error) {
     error.message = "authentication failed";
     res.status(403).json({ error: error.message });
--- controllers/middleware/auth.test.js
+++ controllers/middleware/auth.test.js
@@ -4,7 +4,7 @@ const checkAuth = require("./auth.js");
 describe("Testing middleware functionnalities", () => {
   const token = createToken("test");
 
-  test("checkAuth valid token checkAuth function", () => {
+  test("checkAuth valid token checkAuth function", async () => {
     const mockedReq = () => {
       const req = {
         cookies: {
@@ -26,11 +26,11 @@ describe("Testing middleware functionnalities", () => {
     const req = mockedReq();
     const res = mockedRes();
 
-    checkAuth(req, res, next);
+    await checkAuth(req, res, next);
     expect(next.mock.calls).toHaveLength(1);
   });
 
-  test("checking invalid token checkAuth", () => {
+  test("checking invalid token checkAuth", async () => {
     const mockedReq = () => {
       const req = {
         cookies: {
@@ -49,7 +49,7 @@ describe("Testing middleware functionnalities", () => {
     const next = jest.fn();
     const req = mockedReq();
     const res = mockedRes();
-    checkAuth(req, res, next);
+    await checkAuth(req, res, next);
     expect(next.mock.calls).toHaveLength(0);
     expect(res.status).toHaveBeenCalledWith(403);
     expect(res.json).toHaveBeenCalledWith({
