260a9fb37a23e153ae13a0ec51cd104888876c6d
idriss30 <idriss.cissoko@hotmail.com>
2022-10-22 21:51:30 -0400

    added script to add products in database if they don't exist

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const sequelize = require("./database/config/db.config");
 const app = require("./app");
 const User = require("./database/models/User");
 const Order = require("./database/models/Orders");
+const Product = require("./database/models/Products");
+const items = require("./database/productsData");
 // define the port
 let port = process.env.PORT || 5000;
 
@@ -13,7 +15,9 @@ User.hasMany(Order, { foreignKey: "userId" });
 Order.belongsTo(User);
 sequelize
   .sync()
-  .then(() => {
+  .then(async () => {
+    const products = await Product.findAll();
+    if (products.length == 0) await Product.bulkCreate(items);
     app.listen(port, () => {
       console.log(`server started  on port ${port}`);
       console.log("database__connected");
